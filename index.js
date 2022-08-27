01c37ff3ec02884aaf3078ab68c25c42e70637bb
andre <61792917+luuxis@users.noreply.github.com>
2022-08-28 01:48:01 +0200

    change api url

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const nodeFetch = require('node-fetch');
 
-let api = 'http://127.0.0.1:3000/api';
+let api = 'http://luuxis.fr/api';
 
 async function main() {
     let data = {
-        token: '',
+        pseudo: 'Luuxis',
+        name: 'andre',
+        lastName: 'gallo',
+        email: 'mail@luuxis.fr',
+        password: '123456',
+        admin: true
     }
+
     let response = await nodeFetch(`${api}/signup`, {
         method: 'POST',
         body: JSON.stringify(data)
